3f5908279ab0e0e9cd8e7a031fc127a312162a72
Marcel Verweij <marcelverweij@users.noreply.github.com>
2016-02-15 15:22:09 +0100

    Apply support for daylight saving

--- scripts/sun.py
+++ scripts/sun.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python
 
+import os
 import urllib2
 import json
+import time
+import datetime
 
-# Europe/Netherlands/~Rotterdam
+# Set timezone to The Netherlands
+os.environ['TZ'] = 'Europe/Amsterdam'
+# Europe/Rotterdam
 latitude = "51"
 longitude = "4"
 
-url = 'http://api.sunrise-sunset.org' + \
-	'/json' + \
-	'?lat=' + latitude + \
-	'&lng=' + longitude + \
-	'&date=today'
+def getDatetime(var):
+        var = datetime.datetime.strptime(var, "%I:%M:%S %p")
 
+        today = datetime.date.today()
+        var = var.replace(today.year, today.month, today.day)
+
+        if time.daylight == 1:
+                var = var + datetime.timedelta(hours=1)
+
+        return var
+
+
+url = 'http://api.sunrise-sunset.org' + \
+        '/json' + \
+        '?lat=' + latitude + \
+        '&lng=' + longitude + \
+        '&date=today'
 result = urllib2.urlopen(url).read()
 parsed_json = json.loads(result)
+sunrise = getDatetime(parsed_json['results']['sunrise'])
+sunset  = getDatetime(parsed_json['results']['sunset'])
 
-sunrise = parsed_json['results']['sunrise']
-sunset = parsed_json['results']['sunset']
-print sunrise + ' | ' + sunset
+print sunrise.time(), '|', sunset.time()
